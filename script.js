d1e49c070cd5b9941c92573bcbb9868129e23ffb
anil-gc <anil.11800178@lpu.in>
2024-02-25 17:58:09 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// let var const 
+//function
+// array obj and string
+
+function name() {
+    
+}
+
+const arr1 = ['10','abc','20']; //array
+let obj1 = {'anil':'200',} //obj
+
+
+let a = '10' //string
+
+
+
+function sum(a,b) {
+    console.log(a+b);
+}
+
+function sub(a,b) { //fun declaration
+    console.log(a-b);
+}
+
+sum(2,4);
+
+sub(10,3);
